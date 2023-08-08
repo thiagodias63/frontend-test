327e810bd42e4f838cab5faa61e991d228bbea26
thiago.bittencourt <thidias6003@gmail.com>
2023-08-08 20:18:51 -0300

    feat: breadcrumb pegando último filtro

--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,12 +1,17 @@
 import {Component} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Observable, map} from 'rxjs';
+import {Place} from 'src/app/entities/place/place';
+import * as fromApp from 'src/app/store/app.reducer';
 
 @Component({
   selector: 'breadcrumb',
   template: ` <div class="breadcrumb__container">
     <nz-breadcrumb nzSeparator=">">
       <nz-breadcrumb-item>Início</nz-breadcrumb-item>
-      <nz-breadcrumb-item>
-        <a>Hotéis</a>
+      <nz-breadcrumb-item>Hotéis</nz-breadcrumb-item>
+      <nz-breadcrumb-item *ngIf="destinoSelecionado$ | async as destinoSelecionado">
+        <a>{{ destinoSelecionado.name }}, {{ destinoSelecionado.state.shortname }}</a>
       </nz-breadcrumb-item>
     </nz-breadcrumb>
   </div>`,
@@ -25,4 +30,10 @@ import {Component} from '@angular/core';
     `,
   ],
 })
-export class BreadcrumbComponent {}
+export class BreadcrumbComponent {
+  destinoSelecionado$: Observable<Place | null>;
+
+  constructor(private store: Store<fromApp.AppState>) {
+    this.destinoSelecionado$ = this.store.select('buscarHoteis').pipe(map((buscarHoteisState) => buscarHoteisState.filtroDestino));
+  }
+}
